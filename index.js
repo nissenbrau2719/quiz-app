2a081c6acf2dcbcc730931f73c47fa822d8f8868
Paul <nissenbrau@gmail.com>
2019-11-02 12:17:48 -0600

    added function stubs and pseudocode, updated a few font sizes etc

--- index.js
+++ index.js
@@ -13,3 +13,43 @@
 
 // once all questions are answered, the app should show a final score and ask
 // if you would like to try again with the reset button
+
+function startQuiz() {
+  console.log('ran startQuiz');
+  // this should add an event listener to the start quiz button, and on click
+  // should render the first question/answer set and submit button in the quiz form. 
+  // it should also render the scorekeeping section on the page
+
+}
+
+function submitAnswer() { 
+  console.log('ran submitAnswer');
+  // this should add an event listener to the submit button, and on click should check
+  // to see if the selected answer matches the correct one. it should display an alert 
+  // window notifying if the answer is correct, or one displaying the correct answer 
+  // if it is not correct. the alert window should have the next question button
+
+}
+
+function nextQuestion() {
+  console.log('ran nextQuestion');
+  // this should have an event listener for the next question button. on click, it should
+  // close the alert window and render the next question/answer set in the form. it should
+  // also update the scorekeeping section to reflect the current score and question number
+  // if you have answered all the questions, this should take you to the ending page with
+  // your final score
+
+}
+
+function resetQuiz() {
+  console.log('ran resetQuiz');
+  // this should add an event listener to the reset quiz button. on click, it should take
+  // clear the scorekeeping section and take you back to the starting page of the quiz
+
+}
+
+function makeQuiz() {
+  console.log('ran makeQuiz');
+  //this should run all the previous functions
+
+}
