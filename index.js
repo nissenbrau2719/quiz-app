b2025b65ed98b29272529d739814643ef41bee7c
Paul <nissenbrau@gmail.com>
2019-11-02 10:38:59 -0600

    removed some wireframing elements, updated font and background, wrote user stories

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("js connected");
+// this app should render a welcome screen and a button to start the quiz
+
+// once clicking start, the app should show the first question and a set of
+// 4 options to choose as an answer with a submit button. there should also
+// be a button to reset the quiz.
+
+// the app should also track your score and what question you are on once
+// you have started the quiz. this should update after answering each question
+
+// if you answer a question correctly, render a cheers pic and congrats message
+
+// if you answer a question wrong, render a spilled beer pic and show correct answer
+
+// once all questions are answered, the app should show a final score and ask
+// if you would like to try again with the reset button
